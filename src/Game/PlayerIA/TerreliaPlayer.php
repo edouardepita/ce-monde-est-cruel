7d90a2c07c30bddc81889f55e093741d0bc967cb
Edouard MAHE <edouard.mahe@epita.fr>
2020-10-07 21:50:19 +0200

    commentede

--- src/Game/PlayerIA/TerreliaPlayer.php
+++ src/Game/PlayerIA/TerreliaPlayer.php
@@ -16,12 +16,23 @@ class TerreliaPlayer extends Player
     protected $result;
 
     protected $oppositePlayer;
+    protected $lastchance;
 
+
+    /* 
+
+    1 - Je commence par une pierre
+    2 - Je commence à renvoyer ce qui bat le cout n-1 
+    3 - Ayant commencé par une pierre, je verifie si l'ennemi utilise la meme stratégie (cout 2 : papier ...)
+    4 - Si au bout du 15eme round je ne suis toujours pas gagnant (calcul entre 10 et 15), j'alterne entre retourner ce qui contre le coup n-1 et contrer le contre (pour éviter que la stratégie soit anticipable)
+  
+    */
     public function getChoice()
     {   
 
         if ($this->result->getNbRound() == 0){
             $this->oppositePlayer = false;
+            $this->lastchance = false;
             return parent::rockChoice();
         }
             
@@ -32,7 +43,47 @@ class TerreliaPlayer extends Player
             $this->oppositePlayer = true;
         }
 
+        if ($this->result->getNbRound() == 16) 
+            {
+                $ennemyScore = 0;
+                $myScore = 0;
+                for ($i = 5; $i <= 15; $i++){
+                    $myScore += $this->result->getScoresFor($this->mySide)[$i];
+                    $ennemyScore += $this->result->getScoresFor($this->opponentSide)[$i];
+                }
+                if ($ennemyScore > $myScore){
+                    $lastchance = true;
+                    $oppositePlayer = false;
+                }
+            }
+
+        if ($this->lastchance){
+            if ($this->result->getNbRound() % 2 == 0){
+                if ($this->result->getLastChoiceFor($this->mySide) == 'rock'){
+                    return parent::scissorsChoice();    
+                }
+                if ($this->result->getLastChoiceFor($this->mySide) == 'paper'){
+                    return parent::rockChoice();
+                }
+                if ($this->result->getLastChoiceFor($this->mySide) == 'scissor'){
+                    return parent::paperChoice();
+                }
+            }
+            else{
+                if ($this->result->getLastChoiceFor($this->opponentSide) == 'rock'){
+                    return parent::paperChoice();
+                }
+                if ($this->result->getLastChoiceFor($this->opponentSide) == 'paper'){
+                    return parent::scissorsChoice();
+                }
+                if ($this->result->getLastChoiceFor($this->opponentSide) == 'scissor'){
+                    return parent::rockChoice();
+                }
+            }
+        }
+
         if ($this->oppositePlayer){
+
             if ($this->result->getLastChoiceFor($this->mySide) == 'rock'){
                 return parent::scissorsChoice();    
             }
@@ -86,3 +137,4 @@ class TerreliaPlayer extends Player
 
     }
 };
+    
